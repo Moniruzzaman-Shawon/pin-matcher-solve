5593532d3bdd2544a2d4b461bd738fb1b7136903
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2023-12-26 06:26:14 +0600

    password validation

--- js/app.js
+++ js/app.js
@@ -29,6 +29,26 @@ document.getElementById('key-pad').addEventListener('click', function(event){
     const previousNumber = calcInput.value;
     const newNumber = previousNumber + number; 
     calcInput.value = newNumber;
+
+    
     }
+
     
 })
+
+function verifyPin(){
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const notifySuccess = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+    if(pin == typedNumbers){
+        notifySuccess.style.display = 'block';
+        failError.style.display = 'none';
+    }
+    else{
+        
+        failError.style.display = 'block';
+        notifySuccess.style.display = 'none';
+    }
+    
+}
